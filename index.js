31d39f1c65def74d128c297aa6ff7b4e69beb9c3
Hannah Hudson <hannah.hch@gmail.com>
2021-02-11 16:11:19 -0700

    Add phrases object

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+var phrases = {
+  happy: ["You go!", "Your smile just cheered me up!", "YAY!"],
+  silly: ["Interesting response", "Sounds ... good?", "Me too."],
+  sad: ["Do you want to talk?", "Keep your head up", "We can cry together!", "What can I do?"]
+}
