40230f3f24a18a12884455c63a40798d533a6612
Zach Johnson <zachjjohns@gmail.com>
2021-02-12 12:30:28 -0600

    Initial commit

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ var phrases = {
   silly: ["Interesting response", "Sounds ... good?", "Me too."],
   sad: ["Do you want to talk?", "Keep your head up", "We can cry together!", "What can I do?"]
 }
+
+var happyButton = document.querySelector(".happy");
+var sillyButton = document.querySelector(".silly");
+var cryingButton = document.querySelector(".crying");
+var message = document.querySelector(".message");
+
+
+happyButton.addEventListener("click", getRandomHappy);
+sillyButton.addEventListener("click", getRandomSilly);
+cryingButton.addEventListener("click", getRandomCrying);
+
+function getRandomHappy() {
+  message.innerText = phrases.happy[getRandomIndex(phrases.happy)]
+}
+
+function getRandomSilly(){
+  message.innerText = phrases.silly[getRandomIndex(phrases.silly)]
+}
+
+function getRandomCrying(){
+  message.innerText = phrases.sad[getRandomIndex(phrases.sad)]
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
